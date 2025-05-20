900b456b9288a009a75788609cd5d398194676cb
Kasra <kasra.sh1385@gmail.com>
2025-05-20 23:04:21 +0330

    eyebat sprite added

--- core/src/main/java/com/untilDawn/models/enums/EnemyType.java
+++ core/src/main/java/com/untilDawn/models/enums/EnemyType.java
@@ -1,20 +1,23 @@
 package com.untilDawn.models.enums;
 
 public enum EnemyType {
-    TREE("Tree", 1000, 0, 4, "Images/Enemies/Tree/tree0.png", false);
+    TREE("Tree", 1000, 0, "Images/Enemies/Tree/tree0.png", false),
+    EYEBAT("EyeBat", 50, 2, "Images/Enemies/eyebat/eyebat0.png", true),
+    TENTACLE("TentacleMonster", 25, 1, "Images/Enemies/tentacle/tentacle0.png", true),
+
+    ;
+
 
     private final String name;
     private final int health;
     private final float speed;
-    private final int damage;
     private final String texturePath;
     private final boolean canMove;
 
-    EnemyType(String name, int health, float speed, int damage, String texturePath, boolean canMove) {
+    EnemyType(String name, int health, float speed, String texturePath, boolean canMove) {
         this.name = name;
         this.health = health;
         this.speed = speed;
-        this.damage = damage;
         this.texturePath = texturePath;
         this.canMove = canMove;
     }
@@ -30,11 +33,7 @@ public enum EnemyType {
     public float getSpeed() {
         return speed;
     }
-
-    public int getDamage() {
-        return damage;
-    }
-
+    
     public String getTexturePath() {
         return texturePath;
     }
